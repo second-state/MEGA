9d3bb1178e9e103d953bd4eee0a3fef0e27bede5
Tricster <mediosrity@gmail.com>
2022-10-21 10:12:42 +0800

    Add Transformer Trait.
    Signed-off-by: Tricster <mediosrity@gmail.com>

--- mega_etl/src/lib.rs
+++ mega_etl/src/lib.rs
@@ -1,14 +1,38 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub use async_trait::async_trait;
+pub use mysql_async::prelude::*;
+pub use mysql_async::*;
+use std::sync::{Arc, Mutex};
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum TransformerError {
+    #[error("function is unimplemented")]
+    Unimplemented,
+    #[error("Error: {0}")]
+    Custom(String),
+    #[error("unknown data store error")]
+    Unknown,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub type TransformerResult<T> = std::result::Result<T, TransformerError>;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+enum DataSource {
+    Hyper(String, u16),
+    Redis,
+    Kafka,
+    Unknown,
+}
+
+#[async_trait]
+pub trait Transformer {
+    async fn transform(_inbound_data: Vec<u8>) -> TransformerResult<String> {
+        Err(TransformerError::Unimplemented.into())
+    }
+
+    async fn transform_save(
+        _inbound_data: Vec<u8>,
+        _conn: Arc<Mutex<Conn>>,
+    ) -> TransformerResult<()> {
+        Err(TransformerError::Unimplemented.into())
     }
 }
